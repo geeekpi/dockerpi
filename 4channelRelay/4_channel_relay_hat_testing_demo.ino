db893f64c586bfa421770201d2bbd833437cd2ce
yoyojacky <yoyojacky@users.noreply.github.com>
2018-12-06 15:16:23 +0800

    add relay hat testing demo
    add relay hat testing demo

---
+++ 4channelRelay/4_channel_relay_hat_testing_demo.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+const unsigned char relay[] = {0x01, 0x02, 0x03, 0x04};
+void setup() {
+  Wire.begin();
+}
+
+void loop() {
+  for (int i = 0; i < 4; i++) {
+    Wire.beginTransmission(16);
+    Wire.write(relay[i]);
+    Wire.write(0xff);
+    Wire.endTransmission();
+    delay(200);
+  }
+  delay(500);
+  for (int i = 0; i < 4; i++) {
+    Wire.beginTransmission(16);
+    Wire.write(relay[i]);
+    Wire.write(0x00);
+    Wire.endTransmission();
+    delay(200);
+  }
+  delay(500);
+}
